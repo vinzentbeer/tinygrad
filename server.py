a862440d922330f6d6f16d2301c35b246c1afdb9
qazal <qazal.software@gmail.com>
2024-01-20 08:56:47 +0200

    p2: add POST

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
+import urllib.parse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-  def do_GET(self):
+  def do_POST(self):
+    content_length = int(self.headers['Content-Length'])
+    post_data = self.rfile.read(content_length)
+    parsed_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
+    code = parsed_data.get('code', [''])[0]
+    print("code: {}", code)
+
     self.send_response(200)
     self.send_header("Content-type", "text/plain")
     self.end_headers()
-    self.wfile.write(b"Hello, World!")
+    self.wfile.write(b"Code recieved")
 
 if __name__ == "__main__":
   server_address = ("", 80)
