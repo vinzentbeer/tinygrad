b1a079772e218452f47859588f6f0bf12a3c0d76
qazal <qazal.software@gmail.com>
2024-01-20 09:28:44 +0200

    keep the get

--- server.py
+++ server.py
@@ -4,6 +4,12 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from tinygrad.runtime.ops_hip import compile_hip
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(b'Hello, World!')
+
   def do_POST(self):
     content_length = int(self.headers['Content-Length'])
     post_data = self.rfile.read(content_length)
